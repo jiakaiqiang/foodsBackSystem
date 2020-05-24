93c0c87c141d9f02e252be282933d54ac693c73b
jiakaiqiang <jkq19961125@>
2020-05-24 18:18:53 +0800

    试试

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <el-button type="primary" disabled>主要按钮</el-button>
   <el-button type="success" disabled>成功按钮</el-button>
   <el-button type="info" disabled>信息按钮</el-button>
-  <el-button type="warning" disabled>警告按钮</el-button>
+  <el-button type="warning" disabled>警告ss按钮</el-button>
   <el-button type="danger" disabled>危险按钮</el-button>
 </el-row>
     <img src="./assets/logo.png">
